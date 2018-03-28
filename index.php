e0722f63df1e4c31338327a5ee8414227de6ff3a
Gentille Plume <melvin.lemoine@feather-project.org>
2018-03-28 20:08:02 +0200

    Developed index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Stick Fight: the game</title>
+        
+        <meta charset="utf-8">
+        
+        <!--########## STYLES SHEETS ##########-->
+        
+        <!--TOOLS & RESSOURCES-->
+        <link href="resources/animate.css/animate.css" rel="stylesheet">
+        <link href="resources/fonts/fonts.css" rel="stylesheet">
+        
+        <!--GENERAL PROPERTIES-->
+        <link href="styles/general.css" rel="stylesheet">
+        
+        <!--NAVIGATION BAR-->
+        <link rel="stylesheet" type="text/css" href="styles/navbar/desktop.css">
+        
+        <!--HEADER-->
+        <link rel="stylesheet" type="text/css" href="styles/header/desktop.css">
+        
+        <!--POINTS-->
+        <link rel="stylesheet" type="text/css" href="styles/points/desktop.css">
+        
+        <!--FOOTER-->
+        <link rel="stylesheet" type="text/css" href="styles/footer/desktop.css">     
+        
+    </head>
+
+<!--####################################################################################################-->
+    
+    <body>
+    
+        <!--########## ELEMENTS ##########-->
+        
+        <!--HEADER-->
+        <?php include 'elements/header.html'?>
+        
+        <!--POINTS-->
+        <?php include 'elements/presentation.html'?>
+        
+        <!--FOOTER-->
+        <?php include 'elements/footer.html'?>
+        
+        <!--########## SCRIPTS ##########-->
+        
+        <!--RESOURCES-->
+        <script src="resources/jquery-3.3.1.js"></script>
+        <script src="resources/typed.js/lib/typed.js"></script>
+        
+        <!--OTHER SCRIPTS-->
+        <script src="scripts/sizes.js"></script>
+        <script src="scripts/navbar.js"></script>  
+        
+    </body>
+    
+</html>
