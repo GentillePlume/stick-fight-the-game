66825ce324ef464a9904a21d1694032cc3bc2727
Gentille Plume <melvin.lemoine@feather-project.org>
2018-03-30 11:11:24 +0200

    useless blank line removed

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Stick Fight: the game</title>
-        
-        <meta charset="utf-8">
-        
-        <!--########## STYLES SHEETS ##########-->
-        
-        <!--TOOLS & RESSOURCES-->
-        <link href="resources/animate.css/animate.css" rel="stylesheet">
-        <link href="resources/fonts/fonts.css" rel="stylesheet">
-        
-        <!--GENERAL PROPERTIES-->
-        <link href="styles/general.css" rel="stylesheet">
-        
-        <!--NAVIGATION BAR-->
-        <link rel="stylesheet" type="text/css" href="styles/navbar/desktop.css">
-        
-        <!--HEADER-->
-        <link rel="stylesheet" type="text/css" href="styles/header/desktop.css"> 
-        
-    </head>
+
+<head>
+    <title>Stick Fight: the game</title>
+
+    <meta charset="utf-8">
+
+    <!--########## STYLES SHEETS ##########-->
+
+    <!--TOOLS & RESSOURCES-->
+    <link href="resources/animate.css/animate.css" rel="stylesheet">
+    <link href="resources/fonts/fonts.css" rel="stylesheet">
+
+    <!--GENERAL PROPERTIES-->
+    <link href="styles/general.css" rel="stylesheet">
+
+    <!--NAVIGATION BAR-->
+    <link rel="stylesheet" type="text/css" href="styles/navbar/desktop.css">
+
+    <!--HEADER-->
+    <link rel="stylesheet" type="text/css" href="styles/header/desktop.css">
+
+</head>
 
 <!--####################################################################################################-->
-    
-    <body>
+
+<body>
     
         <!--########## ELEMENTS ##########-->
         
@@ -33,19 +34,19 @@
         
         <!--NAVBAR-->
         <?php include 'elements/navbar.html'?>
-        
-        <!--########## SCRIPTS ##########-->
-        
-        <!--RESOURCES-->
-        <script src="resources/jquery-3.3.1.js"></script>
-        <script src="resources/typed.js/lib/typed.js"></script>
-        
-        <!--OTHER SCRIPTS-->
-        <script src="scripts/sizes.js"></script>
-        <script src="scripts/navbar.js"></script>
-        <script src="scripts/typedtext.js"></script>
-        <script src="scripts/header.js"></script>
-        
-    </body>
-    
+
+    <!--########## SCRIPTS ##########-->
+
+    <!--RESOURCES-->
+    <script src="resources/jquery-3.3.1.js"></script>
+    <script src="resources/typed.js/lib/typed.js"></script>
+
+    <!--OTHER SCRIPTS-->
+    <script src="scripts/sizes.js"></script>
+    <script src="scripts/navbar.js"></script>
+    <script src="scripts/typedtext.js"></script>
+    <script src="scripts/header.js"></script>
+
+</body>
+
 </html>
